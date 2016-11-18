06e7822112f0c3cbd20374fd854b02f1e2e094d7
sergeiserov <sergei-serov@mail.ru>
2016-11-18 11:45:07 +0300

    new file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express';
+import cors from 'cors';
+
+import canonize from './canonize';
+
+const app = express();
+app.use(cors());
+app.get('/task2C', (req, res) => {
+
+  const username = req.query.username || '';
+
+  const testUrl = 'telegram/qweqe23234';
+  const canonizeUserName = canonize(username);
+  res.send(canonizeUserName);
+});
+
+app.listen(3000, () => {
+  console.log('Your app listening on port 3000!');
+});
