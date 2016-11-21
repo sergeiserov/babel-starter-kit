8ff65f12811cb6b983ef74dfab328059861a6afd
sergeiserov <sergei-serov@mail.ru>
2016-11-21 15:43:21 +0300

    task 2D

--- src/index.js
+++ src/index.js
@@ -1,4 +1,48 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express';
+import cors from 'cors';
+import parse from 'parse-color';
+
+const app = express();
+app.use(cors());
+
+const invalidColor = (res => {
+  return res.status(400).send('Invalid color');
+});
+
+app.get('/task2D', (req, res) => {
+
+  let color = req.query.color || '';
+  
+  color = color.trim().toLowerCase();
+  let size = color.length;
+  console.log(color.indexOf('#'));
+    
+  if(color.indexOf('#') > -1) {
+    size = size - 1;
+  } else {
+    color = '#' + color;
+  }
+
+  console.log(color.length + color);
+  
+  if(!size == 3  && !size == 6){
+    invalidColor(res);
+    return;
+  }
+
+
+  const hex = parse(color).hex;
+  console.log(hex);
+
+  if(!hex) {
+    invalidColor(res);
+    //res.status(400).send('Invalid color');
+  }else{
+    res.status(200).send(hex);
+  }
+
+});
+
+app.listen(3000, () => {
+  console.log('Your app listening on port 3000!');
+});
